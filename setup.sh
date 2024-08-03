a3aa19094faa5f7c3537d58e4d36ce46a7a365a3
castlele <castlelecs@gmail.com>
2024-08-03 14:49:19 +0300

    [feat]: update setup script for macos

--- setup.sh
+++ setup.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-sudo apt install tmux
+OS_TYPE="$OSTYPE"
+LINUX_MINT="linux-gnu"
+MACOS="darwin"
+
+if [[ $OS_TYPE == $LINUX_MINT* ]]; then
+    INSTALLATION_CMD="sudo apt install"
+elif [[ $OS_TYPE == $MACOS* ]]; then
+    INSTALLATION_CMD="brew install"
+fi
+
+$INSTALLATION_CMD tmux
