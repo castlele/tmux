f1ae2ba68e5b937b0e05ad73299a0e3f1045381c
castlele <castlelecs@gmail.com>
2026-03-02 19:53:05 +0300

    feat(init): update init script to not open notes

--- init_session.sh
+++ init_session.sh
@@ -6,9 +6,6 @@ SESSION_EXISTS=$(tmux list-sessions | grep $SESSION)
 
 if [[ "$SESSION_EXISTS" = "" ]]; then
     tmux new-session -d -s $SESSION
-
-    tmux rename-window -t 1 'NOTES'
-    tmux send-keys -t 'NOTES' 'notes' C-m
 fi
 
 tmux attach-session -t $SESSION
